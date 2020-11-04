14d24ae466b9d9f8815cc192fa514a2479934c7b
Slogreco <shoshwright@hotmail.com>
2020-11-04 12:54:06 -0700

    set up server

---
+++ employeeTracker.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "",
+    database: "employee_DB"
+});
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+
+});
