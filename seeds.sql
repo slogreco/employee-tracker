0ec6d3692c99de7015a784fae6ed36c1d7aa4b10
Slogreco <shoshwright@hotmail.com>
2020-11-04 13:23:28 -0700

    added seeds

---
+++ seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("David", "Rose", 5, 1), ("Alexis", "Rose", 4, 1), ("Stevie", "Budd", 3, 1);
+
+INSERT INTO department (department_name)
+VALUES ("Floral"), ("Party Planning"), ("Front Desk");
+
+INSERT INTO employee_role (title, salary, department_id)
+VALUES ("Florist", 40000, 1), ("Receptionist", 25000, 2), ("Drunk", 100000, 3);
+
