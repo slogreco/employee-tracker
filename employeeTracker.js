e84f52271fd574e31d6090b0af89f78b96d33007
Slogreco <shoshwright@hotmail.com>
2020-11-04 20:28:15 -0700

    created add function

--- employeeTracker.js
+++ employeeTracker.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
     // Your username
     user: "root",
     // Your password
-    password: "",
+    password: "N0va!sql",
     database: "employees_DB"
 });
 
@@ -48,9 +48,107 @@ function add() {
     inquirer
         .prompt([
             {
-
+                name: "view",
+                type: "list",
+                message: "What would you like to add?",
+                choices: ["Employee", "Department", "Role"]
             }
         ])
+        .then(function (answer) {
+            if (answer.view === "Employee") {
+                inquirer.prompt([
+                    {
+                        name: "firstName",
+                        type: "input",
+                        message: "What is the employees first name?"
+                    },
+                    {
+                        name: "lastName",
+                        type: "input",
+                        message: "What is the employees last name?"
+                    },
+                    {
+                        name: "roleId",
+                        type: "input",
+                        message: "What is the employees role id?"
+                    },
+                    {
+                        name: "managerId",
+                        type: "input",
+                        message: "What is the employees manager id?"
+                    }
+                ]).then(function (answer) {
+                    connection.query(
+                        "INSERT INTO employee SET ?",
+                        {
+                            first_name: answer.firstName,
+                            last_name: answer.lastName,
+                            role_id: answer.roleId,
+                            manager_id: answer.managerId
+                        },
+                        function (err) {
+                            if (err) throw err;
+                            console.log("Employee created sucessfully!");
+                            start();
+                        }
+                    )
+                })
+            }
+            else if (answer.view === "Department") {
+                inquirer.prompt([
+                    {
+                        name: "departmentName",
+                        type: "input",
+                        message: "What is the department name?"
+                    }
+                ]).then(function (answer) {
+                    connection.query(
+                        "INSERT INTO department SET ?",
+                        {
+                            department_name: answer.departmentName,
+                        },
+                        function (err) {
+                            if (err) throw err;
+                            console.log("Department created sucessfully!");
+                            start();
+                        }
+                    )
+                })
+            }
+            else if (answer.view === "Role") {
+                inquirer.prompt([
+                    {
+                        name: "title",
+                        type: "input",
+                        message: "What is the employees title?"
+                    },
+                    {
+                        name: "salary",
+                        type: "input",
+                        message: "What is the employees salary?"
+                    },
+                    {
+                        name: "id",
+                        type: "input",
+                        message: "What is the employees department id?"
+                    }
+                ]).then(function (answer) {
+                    connection.query(
+                        "INSERT INTO employee_role SET ?",
+                        {
+                            title: answer.title,
+                            salary: answer.salary,
+                            department_id: answer.id
+                        },
+                        function (err) {
+                            if (err) throw err;
+                            console.log("Employee role created sucessfully!");
+                            start();
+                        }
+                    )
+                })
+            }
+        });
 }
 // Update employee roles
 function update() {
